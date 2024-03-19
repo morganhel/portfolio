6ff81e00d46eb9973bb81650e1f778f9ea3a3519
morganhel <138800018+morganhel@users.noreply.github.com>
2024-03-19 14:09:48 +0100

    Problème affichage main après déploiement

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
     <Router>
         <Header />
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
       </Router>
