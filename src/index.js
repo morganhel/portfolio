ab7deb280b57fb45717c7dde865cf9628ca79c5e
morganhel <138800018+morganhel@users.noreply.github.com>
2024-03-22 11:36:31 +0100

    essai déploiement gh-pages

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
       <Header />
-      <Home />
-        {/* <Route path="/" element={<Home />} /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
+    </Router>
   </React.StrictMode>
 );
 
