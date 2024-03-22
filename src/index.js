e6ddc33851affaf2cc42df806fcf757edad01bae
morganhel <138800018+morganhel@users.noreply.github.com>
2024-03-22 12:00:20 +0100

    essai déploiement

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/portfolio'>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
