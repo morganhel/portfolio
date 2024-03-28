f87c385f9df5848e78e68135eba9e403c77b0265
morganhel <138800018+morganhel@users.noreply.github.com>
2024-03-28 21:59:18 +0100

    Correction form

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,8 +1,13 @@
 import React, { useRef } from 'react';
+import {useState} from 'react';
 import emailjs from '@emailjs/browser';
 
 function Form() {
   const form = useRef();
+
+  const [nom, setNom]=useState('');
+  const [email, setEmail]=useState('');
+  const [message, setMessage]=useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -19,15 +24,19 @@ function Form() {
           console.log('FAILED...', error.text);
         },
       );
+
+    alert('Message envoyé avec succés');
+
+    setNom('');setEmail('');setMessage('');
   };
 
   return (
     <form ref={form} onSubmit={sendEmail} className='contactForm'>
       <h3 className='contactForm__title'> ENVOYER UN MAIL </h3>
       <div className='contactForm__content'>
-        <input className='contactForm__content--input' type="text" name="user_name" placeholder='NOM'/>
-        <input className='contactForm__content--input' type="email" name="user_email" placeholder='EMAIL'/>
-        <textarea className='contactForm__content--input' name="message" placeholder='MESSAGE'/>
+        <input className='contactForm__content--input' type="text" name="user_name" placeholder='NOM' value={nom} onChange={(e)=>setNom(e.target.value)} required/>
+        <input className='contactForm__content--input' type="email" name="user_email" placeholder='EMAIL' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+        <textarea className='contactForm__content--input' name="message" placeholder='MESSAGE' value={message} onChange={(e)=>setMessage(e.target.value)} required/>
         <input className='contactForm__content--input button' type="submit" value="ENVOYER" />
       </div>
     </form>
