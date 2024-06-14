57f18f9f2923aed1e2837919411f90b826094bdc
morganhel <138800018+morganhel@users.noreply.github.com>
2024-06-14 14:53:32 +0200

    refonte formulaire

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -8,6 +8,7 @@ function Form() {
   const [nom, setNom]=useState('');
   const [email, setEmail]=useState('');
   const [message, setMessage]=useState('');
+  const [offre, setOffre]=useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -27,15 +28,21 @@ function Form() {
 
     alert('Message envoyé avec succés');
 
-    setNom('');setEmail('');setMessage('');
+    setNom('');setEmail('');setMessage('');setOffre('');
   };
 
   return (
     <form ref={form} onSubmit={sendEmail} className='contactForm'>
-      <h3 className='contactForm__title'> ENVOYER UN MAIL </h3>
+      <h3 className='contactForm__title'> PRENEZ CONTACT ! </h3>
       <div className='contactForm__content'>
-        <input className='contactForm__content--input' type="text" name="user_name" placeholder='NOM' value={nom} onChange={(e)=>setNom(e.target.value)} required/>
-        <input className='contactForm__content--input' type="email" name="user_email" placeholder='EMAIL' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+        <input className='contactForm__content--input' type="text" name="nom" placeholder='NOM' value={nom} onChange={(e)=>setNom(e.target.value)} required/>
+        <input className='contactForm__content--input' type="email" name="email" placeholder='EMAIL' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+        <select className='contactForm__content--input' name="offre"  value={offre} onChange={(e)=>setOffre(e.target.value)}>
+          <option>CREATION</option>
+          <option>MAINTENANCE</option>
+          <option>OPTIMISATION</option>
+          <option>CONTACT</option>
+        </select>
         <textarea className='contactForm__content--input' name="message" placeholder='MESSAGE' value={message} onChange={(e)=>setMessage(e.target.value)} required/>
         <input className='contactForm__content--input button' type="submit" value="ENVOYER" />
       </div>
