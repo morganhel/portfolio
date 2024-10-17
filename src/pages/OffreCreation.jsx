ff32a7ef4ac98ab4c45a64f89a29a7b47f7da397
morganhel <138800018+morganhel@users.noreply.github.com>
2024-10-17 21:55:15 +0200

    1ère section page creation

--- src/pages/OffreCreation.jsx
+++ src/pages/OffreCreation.jsx
@@ -7,21 +7,82 @@ function OffreCreation() {
         <>
         <Header />
         <h1>CREATION DE VOTRE SITE VITRINE</h1>
-        <section>
-            <h2>CE QUE JE VOUS PROPOSE !</h2>
-            <p>Mettez votre activité en lumière avec un site vitrine unique, moderne et optimisé pour capter l'attention de vos clients.</p>
-            <p>Je crée des sites qui reflètent votre identité et mettent en avant vos services de façon claire et attractive.
-                <br />Ce que vous obtenez :
-                <br />Design personnalisé pour votre marque.
-                <br />Structure claire et intuitive pour vos visiteurs.
-                <br />Optimisation SEO de base pour améliorer votre visibilité.
-                <br />Site responsive, adapté à tous les appareils.
-                <br />Intégration des fonctionnalités essentielles (formulaire de contact, galeries, etc.).
-            </p>
-            <p>Ne laissez pas votre présence en ligne au hasard – démarquez-vous dès aujourd’hui avec un site vitrine sur mesure !</p>
+        <section className='formules'>
+            <h2 className='formules__title'>QU'EST CE QUE JE VOUS PROPOSE ?</h2>
+            <p className='formules__content--center'>Mettez votre activité en lumière avec un site vitrine unique, moderne et optimisé pour capter l'attention de vos clients.</p>
+            <p>Je crée des sites qui reflètent votre identité et mettent en avant vos services de façon claire et attractive.</p>
+                <p><strong>Ce que vous obtenez :</strong></p>
+                <p className='formules__content--point'>
+                ✿ Design personnalisé pour votre marque.
+                    <br />✿ Structure claire et intuitive pour vos visiteurs.
+                    <br />✿ Optimisation SEO de base pour améliorer votre visibilité.
+                    <br />✿ Site responsive, adapté à tous les appareils.
+                    <br />✿ Intégration des fonctionnalités essentielles (formulaire de contact, galeries, etc.).
+                </p>
+            <p className='formules__content--center formules__content--center--color'>Ne laissez pas votre présence en ligne au hasard – démarquez-vous dès aujourd’hui avec un site vitrine sur mesure !</p>
             <div></div>
         </section>
-        
+        <section>
+            <h2>COMMENT CA SE DEROULE ?</h2>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p>La création d'un site vitrine commence toujours par une discussion approfondie avec vous. Je prends le temps de comprendre votre activité, vos objectifs, et votre public cible. Que vous ayez une idée précise ou non, je suis là pour vous conseiller et vous aider à définir ce que vous attendez de votre site. </p>
+                    <p>Ce que nous abordons ensemble :
+                        <br />Objectifs du site : Attirer des clients, présenter vos services, ou simplement améliorer votre visibilité en ligne.
+                        <br />Public cible : Qui sont vos visiteurs ? Quelles informations doivent-ils trouver rapidement ?
+                        <br />Contenus essentiels : Textes, images, vidéos, ou autres éléments que vous souhaitez mettre en avant.
+                        <br />Design : Couleurs, styles visuels ou fonctionnalités que vous imaginez pour votre site. 
+                        </p>
+                </div>
+            </article>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+            <article>
+                <img />
+                <div>
+                    <h3>1. Ecoute et Analyse de VOS Besoins</h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+            </article>
+        </section>
         </>
     )
 }
